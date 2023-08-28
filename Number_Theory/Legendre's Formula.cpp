d8d9f47a2a3403a2277ab36217cd74fcf4e3888c
pauldipto <143004091+pauldipto@users.noreply.github.com>
2023-08-28 19:14:15 +0600

    Create Legendre's Formula.cpp

---
+++ Number_Theory/Legendre's Formula.cpp
@@ -0,0 +1,45 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastread()          (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+
+
+
+int largestPowerOfK(int n, int k) {
+    
+    map<int, int> factors;
+    for(int i = 2; i * i <= k; i++) {
+        while(k % i == 0) {
+            factors[i]++;
+            k /= i;
+        }
+    }
+    if(k > 1)
+    factors[k]++;
+    
+    int ans = INT_MAX;
+    for(auto it : factors) {
+        int prime = it.first;
+        int count = it.second;
+        int power = 0;
+        for(int i = prime; i <= n; i *= prime) {
+            power += n/i;
+        }
+        ans = min(ans, power / count);
+    }
+    return ans;
+}
+
+int main()
+{
+    fastread();
+    
+    int n, k;
+    cin >> n >> k;
+    
+    cout << largestPowerOfK(n, k) << endl;
+    
+     
+    
+}
