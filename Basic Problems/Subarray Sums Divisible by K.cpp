97146cf724e94d4c4e1470f1ac4de539f36b364f
pauldipto <143004091+pauldipto@users.noreply.github.com>
2023-09-04 23:56:49 +0600

    Update Subarray Sums Divisible by K.cpp

--- Basic Problems/Subarray Sums Divisible by K.cpp
+++ Basic Problems/Subarray Sums Divisible by K.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int subarraysDivByK(vector<int> nums, int k) {
     int count = 0;
     int sum = 0;
-    unordered_map<int, int> m;
+    unordered_map<int, int> mp;
     m[0]++;
     
     for(auto i : nums) {
         sum += i;
-        if(m[(sum % k + k) % k] > 0) {
-            count += m[(sum % k + k) % k];
+        if(mp[(sum % k + k) % k] > 0) {
+            count += mp[(sum % k + k) % k];
         }
-        m[(sum % k + k) % k]++;
+        mp[(sum % k + k) % k]++;
     }
     return count;
 }
