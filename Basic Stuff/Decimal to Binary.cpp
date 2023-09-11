6dabb24a42e0b819467883366e9c70189201799e
pauldipto <143004091+pauldipto@users.noreply.github.com>
2023-09-12 01:55:13 +0600

    Update Decimal to Binary.cpp

--- Basic Stuff/Decimal to Binary.cpp
+++ Basic Stuff/Decimal to Binary.cpp
@@ -22,7 +22,7 @@ int main()
     int i = 0;
     while(n != 0) {
         int bit = n & 1;
-        ans = (bit * pow(10, i)) + ans;
+        ans += (bit * pow(10, i));
         n >>= 1;
         i++;
     }
