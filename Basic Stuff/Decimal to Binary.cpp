fdd90a236e794135a953a2a3cfeff0dd339fc6a3
pauldipto <143004091+pauldipto@users.noreply.github.com>
2023-09-12 01:44:52 +0600

    Create Decimal to Binary.cpp

---
+++ Basic Stuff/Decimal to Binary.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N                   1e7 + 10
+#define ll                  long long
+#define mod                 1000000007
+#define pb                  push_back
+#define mp                  make_pair
+#define endl                '\n'
+#define fastread()          (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+
+
+int main()
+{
+    fastread();
+    
+    int n;
+    cin >> n;
+    
+    int ans = 0;
+    int i = 0;
+    while(n != 0) {
+        int bit = n & 1;
+        ans = (bit * pow(10, i)) + ans;
+        n >>= 1;
+        i++;
+    }
+    
+    cout << ans << endl;
+    
+    for(int i = 10; i >= 0; i--) {
+        cout << ((n >> i) & 1);
+    }
+    
+    
+    
+    
+    
+}
