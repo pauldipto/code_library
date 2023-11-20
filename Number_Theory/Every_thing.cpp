a20b70912db73b8392aadbb44f91361ce53a0b56
pauldipto <143004091+pauldipto@users.noreply.github.com>
2023-11-20 11:31:55 +0600

    Create Every_thing.cpp

---
+++ Number_Theory/Every_thing.cpp
@@ -0,0 +1,81 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll                  long long
+#define mod                 1000000007
+#define pb                  push_back
+#define mp                  make_pair
+#define endl                '\n'
+#define fastread()          (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+
+
+const int N = 1e6+9;
+int spf[N];
+int gpf[N];
+
+int main()
+{
+    fastread();
+    
+    
+    for(int i = 2; i < N; i++) {
+        spf[i] = i;
+    }
+    
+    for(int i = 2; i < N; i++) {
+        if(spf[i] == i) {
+            for(int j = i; j < N; j += i) {
+                spf[j] = min(spf[j], i);
+                gpf[j] = i;
+            }
+        }
+    }
+    
+    // cout<<spf[22]<<endl;
+    // cout<<gpf[22]<<endl;
+    
+    int n;
+    cin>>n;
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin>>x;
+        
+        cout<<spf[x]<<" ";
+       
+        cout<<gcf[x]<<" ";
+        
+        
+        int distinct_prime_factors = 0;
+        int total_prime_factors = 0;
+        int number_of_divisors = 1;
+        ll sum_of_divisors = 1;
+        while(x > 1) {
+            int k = spf[x];
+            distinct_prime_factors++;
+            int power_of_k = 0;
+            int prime_power = 1;
+            
+            while(x % k == 0) {
+                total_prime_factors++;
+                ++power_of_k;
+                prime_power *= k;
+                x /= k;
+            }
+            
+            number_of_divisors *= (power_of_k + 1);
+            sum_of_divisors *= ( 1LL * prime_power * k - 1) / (k - 1);
+        }
+        
+       
+       cout<<distinct_prime_factors<<" ";
+       
+       cout<<total_prime_factors<<" ";  
+       
+       cout<<number_of_divisors<<" ";
+       
+       cout<<sum_of_divisors<<'\n';     
+        
+    }
+     
+}
